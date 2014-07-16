1f5ff000c441ac6ecf4e554547414b252129f838
Zhen Li <dao.yi.er.san@gmail.com>
2014-07-16 10:42:01 -0400

    Change calib logic and save boxLimit to file

--- calibFileManager.py
+++ calibFileManager.py
@@ -8,10 +8,11 @@ class CalibFileManager:
         self.calibJSON = ''
         self.calibDic = {}
 
-    def write(self, mouseModeValue, clickValue):
+    def write(self, mouseModeValue, clickValue, boxLimit):
         self.calibDic = {}
         self.calibDic['mouseModeValue'] = mouseModeValue
         self.calibDic['clickValue'] = clickValue
+        self.calibDic['boxLimit'] = boxLimit
         #print 'Write Dic: ' + str(calibDic)
         self.calibJSON = json.dumps(self.calibDic)
         print 'Write JSON: ' + str(self.calibJSON)
