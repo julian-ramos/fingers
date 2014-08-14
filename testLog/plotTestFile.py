38bdfca306b4e54c65cafdb1ad88f1d41bf5a409
Zhen <dao.yi.er.san@gmail.com>
2014-08-14 16:29:10 -0400

    add mouse_flg into test data file

--- testLog/plotTestFile.py
+++ testLog/plotTestFile.py
@@ -33,14 +33,15 @@ def run():
 
             # Plot all data
             keys = ['dista0', 'distClick0', 'inRange', 'inBox', 'tIX', 'tIY', 'kIX', 'kIY', \
-            'tTX', 'tTY', 'kTX', 'kTY', 'smoothX', 'smoothY', 'mouseState', 'clickX', 'clickY']
+            'tTX', 'tTY', 'kTX', 'kTY', 'smoothX', 'smoothY', 'mouse_flg', 'mouseState', 'clickX', 'clickY']
             # for i in range(len(keys)):
-            #     figure(fi + i)
+            #     figure(fi)
+            #     fi += 1
             #     plot(data['time'], data[keys[i]], color[i % len(color)])
             #     title('{}-{}'.format(str(fileName), str(keys[i])))
 
             # Plot selected data
-            selData = data[0:1000]
+            selData = data[1000:2000]
             keyX = ['distClick0', 'tIX', 'kIX', 'tTX', 'kTX', 'smoothX', 'mouseState', 'clickX']
             figure(fi)
             fi += 1
@@ -51,7 +52,7 @@ def run():
                 else:
                     plot(selData['time'], selData[keyX[i]], color[i % len(color)], label = keyX[i])
             title('{}-{}'.format(str(fileName), 'X'))
-            legend(loc = 'upper right')
+            legend(loc = 'lower right')
 
             keyY = ['distClick0', 'tIY', 'kIY', 'tTY', 'kTY', 'smoothY', 'mouseState', 'clickY']
             figure(fi)
@@ -62,11 +63,27 @@ def run():
                     plot(selData['time'], ms, color[i % len(color)], label = keyY[i])
                 else:
                     plot(selData['time'], selData[keyY[i]], color[i % len(color)], label = keyY[i])
+            
+            # Plot diff
+            ledDiffMean = (np.diff(selData['tIY']) + np.diff(selData['kIY']) + np.diff(selData['tTY']) + np.diff(selData['kTY'])) / 4 * 100
+            diffTime = selData['time'][1:]
+            # plot(diffTime, ledDiffMean, 'ro-', label = 'diff * 100')
+
+            tmp = np.sum(ledDiffMean[0:5])
+            diffBuff  = np.zeros(len(ledDiffMean) - 4)
+            diffBuff[0] = tmp / 5
+            for i in range(5, len(ledDiffMean)):
+                tmp = tmp - ledDiffMean[i-5] + ledDiffMean[i]
+                diffBuff[i - 4] = tmp / 5
+            buffTime = selData['time'][5:]
+            plot(buffTime, diffBuff, 'b-', label = 'diff buff * 100')
+
+            diffThumbIndex = np.abs((np.diff(selData['tTY']) + np.diff(selData['kTY']) - np.diff(selData['tIY']) - np.diff(selData['kIY'])) / 4 * 100)
+            plot(diffTime, diffThumbIndex, 'r-', label = '(thumbDiff - indexDiff) * 100')
+
             title('{}-{}'.format(str(fileName), 'Y'))
-            legend(loc = 'upper right')
+            legend(loc = 'lower right')
 
             show()
-
-            fi += 1
 
 run()
